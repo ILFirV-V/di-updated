fcacd8ed4b22b1a8207d8407b98ea8d0c4068e0e
Ilfir Sakhabutdinov <ilfir.sakhabutdinov.study@mail.ru>
2024-12-23 03:48:15 +0500

    Add. Более подробное описание опции в `HelpText`

--- TagsCloudContainer.ConsoleUi/Options/WordSettingsOptions.cs
+++ TagsCloudContainer.ConsoleUi/Options/WordSettingsOptions.cs
@@ -6,6 +6,6 @@ namespace TagsCloudContainer.ConsoleUi.Options;
 [Verb("words", HelpText = "Настройки анализа слов")]
 public class WordSettingsOptions : IOptions
 {
-    [Option('v', "valid", HelpText = "Валидные части речи")]
+    [Option('v', "valid", HelpText = "Валидные части речи(V - гл, S - сущ, A - прил, ADV - наречие, NUM - числ). Больше тут: `https://yandex.ru/dev/mystem/doc/ru/grammemes-values`")]
     public ICollection<string> ValidSpeechParts { get; set; } = new List<string>();
 }
