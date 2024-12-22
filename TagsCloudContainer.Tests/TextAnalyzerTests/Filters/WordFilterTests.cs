6555faf1b3181e5ded6b02a9dee565f715704039
Ilfir Sakhabutdinov <ilfir.sakhabutdinov.study@mail.ru>
2024-12-22 23:56:52 +0500

    Add. Тесты для проекта "TextAnalyzer"

---
+++ TagsCloudContainer.Tests/TextAnalyzerTests/Filters/WordFilterTests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using TagsCloudContainer.TextAnalyzer.Logic.Filters;
+using TagsCloudContainer.TextAnalyzer.Models;
+
+namespace TagsCloudContainer.Tests.TextAnalyzerTests.Filters;
+
+[TestFixture]
+[TestOf(typeof(WordFilter))]
+public class WordFilterTests
+{
+    private static readonly WordSettings DefaultSettings = new();
+    private static readonly string startWordFake = string.Empty;
+    private static readonly string formatedWordFake = string.Empty;
+
+    private static IReadOnlyCollection<TestCaseData> validTestCases =
+    [
+        new TestCaseData("V", DefaultSettings)
+            .SetName("Verb"),
+        new TestCaseData("S", DefaultSettings)
+            .SetName("Noun"),
+        new TestCaseData("A", DefaultSettings)
+            .SetName("Adjective"),
+        new TestCaseData("ADV", DefaultSettings)
+            .SetName("Adverb"),
+        new TestCaseData("NUM", DefaultSettings)
+            .SetName("Numeral"),
+        new TestCaseData("N", new WordSettings {ValidSpeechParts = ["N"]})
+            .SetName("CustomSettings"),
+    ];
+
+    private static IEnumerable<TestCaseData> notValidTestCases =
+    [
+        new TestCaseData("invalid", DefaultSettings)
+            .SetName("InvalidSpeechPart"),
+        new TestCaseData(null, DefaultSettings)
+            .SetName("NullSpeechPart"),
+        new TestCaseData(" ", DefaultSettings)
+            .SetName("WhitespaceSpeechPart"),
+        new TestCaseData("V", new WordSettings {ValidSpeechParts = ["S"]})
+            .SetName("WithoutSpeechPart"),
+    ];
+
+    [Test]
+    [TestCaseSource(nameof(validTestCases))]
+    public void IsVerify_Should_BeVerify(string expectedSpeechPart, WordSettings settings)
+    {
+        var filter = new WordFilter();
+        var wordDetails = new WordDetails(startWordFake, formatedWordFake, expectedSpeechPart);
+
+        var result = filter.IsVerify(wordDetails, settings);
+
+        result.Should().BeTrue();
+    }
+
+    [Test]
+    [TestCaseSource(nameof(notValidTestCases))]
+    public void IsVerify_Should_BeNotVerify(string expectedSpeechPart, WordSettings settings)
+    {
+        var filter = new WordFilter();
+        var wordDetails = new WordDetails(startWordFake, formatedWordFake, expectedSpeechPart);
+
+        var result = filter.IsVerify(wordDetails, settings);
+
+        result.Should().BeFalse();
+    }
+}
---
+++ TagsCloudContainer.Tests/TextAnalyzerTests/WordAnalyzerTests/WordAnalyzerTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using MyStemWrapper;
+using TagsCloudContainer.TextAnalyzer.Logic.Analyzers;
+using TagsCloudContainer.TextAnalyzer.Models;
+
+namespace TagsCloudContainer.Tests.TextAnalyzerTests.WordAnalyzerTests;
+
+[TestFixture]
+[TestOf(typeof(WordAnalyzer))]
+public partial class WordAnalyzerTests
+{
+    private MyStem myStem;
+
+    [SetUp]
+    public void SetUp()
+    {
+        myStem = new MyStem
+        {
+            PathToMyStem = "D:\\education\\contur\\di\\mystem.exe",
+            Parameters = "-nli"
+        };
+    }
+
+    [Test]
+    [TestCaseSource(nameof(validWordsTestCases))]
+    public void AnalyzeWordOrNull_Should_HaveExpectedWordDetails(string word, WordDetails expectedWordDetails)
+    {
+        var analyzer = new WordAnalyzer(myStem);
+
+        var result = analyzer.AnalyzeWordOrNull(word);
+
+        result.Should().BeEquivalentTo(expectedWordDetails);
+    }
+
+    [Test]
+    [TestCaseSource(nameof(notValidWordsTestCases))]
+    public void AnalyzeWordOrNull_Should_BeNull(string word)
+    {
+        var analyzer = new WordAnalyzer(myStem);
+
+        var result = analyzer.AnalyzeWordOrNull(word);
+
+        result.Should().BeNull();
+    }
+}
